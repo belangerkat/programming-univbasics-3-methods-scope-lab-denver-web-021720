8f667b98ead00ea25ed2b0ba6d4b4792ebdba782
Katie Belanger <belangerkat@gmail.com>
2020-02-06 22:41:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,3 +8,8 @@ status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def toadstool
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
